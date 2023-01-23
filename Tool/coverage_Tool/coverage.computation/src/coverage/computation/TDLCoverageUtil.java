6cea1820179337da6c345c87bc6945a40f471048
Faezeh Khorram <khorram.faezeh@gmail.com>
2023-01-23 15:52:32 +0100

    update based on using Paths

--- Tool/coverage_Tool/coverage.computation/src/coverage/computation/TDLCoverageUtil.java
+++ Tool/coverage_Tool/coverage.computation/src/coverage/computation/TDLCoverageUtil.java
@@ -1,17 +1,12 @@
 package coverage.computation;
 
-import java.lang.reflect.Method;
+import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import org.eclipse.core.runtime.IConfigurationElement;
-import org.eclipse.core.runtime.Platform;
-import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EAnnotation;
 import org.eclipse.emf.ecore.EClass;
@@ -19,29 +14,21 @@ import org.eclipse.emf.ecore.EClassifier;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.emf.ecoretools.ale.BehavioredClass;
-import org.eclipse.emf.ecoretools.ale.Operation;
-import org.eclipse.emf.ecoretools.ale.Tag;
-import org.eclipse.emf.ecoretools.ale.Unit;
-import org.eclipse.gemoc.dsl.Dsl;
-import org.osgi.framework.Bundle;
+import org.imt.tdl.utilities.DSLProcessor;
 
 import DSLSpecificCoverage.DomainSpecificCoverage;
 import coverage.dslSpecific.DSLSpecificCoverageHandler;
 import coverage.dslSpecific.IDSLSpecificCoverage;
-import fr.inria.diverse.k3.al.annotationprocessor.Aspect;
-import fr.inria.diverse.k3.al.annotationprocessor.Step;
 
 public class TDLCoverageUtil {
 	
-	private static TDLCoverageUtil instance = new TDLCoverageUtil();
+private static TDLCoverageUtil instance = new TDLCoverageUtil();
 	
-	private String DSLPath;
+	private DSLProcessor dslProcessor;
+	private Path DSLPath;
 	private EPackage metamodelRootElement;
 	private Set<String> coverableClasses = new HashSet<>();
-	private Set<String> extendedClassesWithStep = new HashSet<>();
 	private Set<String> extendedClassesWithoutStep = new HashSet<>();
 
 	private List<EClass> classesWithDynamicFeatures = new ArrayList<>();
@@ -71,111 +58,35 @@ public class TDLCoverageUtil {
 		testSuiteCoverage = coverage;
 	}
 
-	public String getDSLPath() {
+	public Path getDSLPath() {
 		return DSLPath;
 	}
 
-	public void setDSLPath(String DSLPath) {
+	public void setDSLPath(Path DSLPath) {
 		instance.DSLPath = DSLPath;
 	}
 	
 	public void runCoverageComputation() {
 		coverableClasses.clear();
-		extendedClassesWithStep.clear();
 		extendedClassesWithoutStep.clear();
 		dynamicClasses.clear();
 		classesWithDynamicFeatures.clear();
 		dslSpecificCoverage = null;
 		dslSpecificCoverageExtension = null;
 		
+		dslProcessor = new DSLProcessor(DSLPath);
+		dslProcessor.loadDSLMetaclasses();
+		metamodelRootElement = dslProcessor.getMetamodelRootElement();
 		findCoverableClassesFromDSLSemantics();
+		
 		testSuiteCoverage.calculateTSCoverage();
 	}
 	
 	private void findCoverableClassesFromDSLSemantics(){
-		final ResourceSet resSet = new ResourceSetImpl();
-		IConfigurationElement language = Arrays
-				.asList(Platform.getExtensionRegistry()
-						.getConfigurationElementsFor("org.eclipse.gemoc.gemoc_language_workbench.xdsml"))
-				.stream().filter(l -> l.getAttribute("xdsmlFilePath").equals(DSLPath.substring(16)))
-				.findFirst().orElse(null);
-		
-		final Resource res = resSet.getResource(URI.createURI(DSLPath), true);
-		final Dsl dsl = (Dsl) res.getContents().get(0);
-		final Bundle bundle = Platform.getBundle(language.getContributor().getName());
-		String ecoreFilePath = dsl.getEntry("ecore").getValue().replaceFirst("resource", "plugin");
-		Resource ecoreResource = (new ResourceSetImpl()).getResource(URI.createURI(ecoreFilePath), true);
-		metamodelRootElement = (EPackage) ecoreResource.getContents().get(0);
-		
-		if (dsl.getEntry("k3") != null) {
-			findK3Classes(dsl, bundle);
-		}else if (dsl.getEntry("ale") != null) {
-			findAleClasses(dsl, bundle);
-		}
+		dslProcessor.findDSLExtendedClasses();
+		extendedClassesWithoutStep.addAll(dslProcessor.getExtendedClassesWithoutStep());
+		coverableClasses.addAll(dslProcessor.getExtendedClassesWithStep());
 		checkInheritanceForNotCoverableClasses();
-	}
-
-	private void findK3Classes(Dsl dsl, Bundle bundle) {
-		final List<Class<?>> classes = dsl.getEntries().stream().filter(e -> e.getKey().equals("k3"))
-				.findFirst()
-				.map(os -> Arrays.asList(os.getValue().split(",")).stream().map(cn -> loadClass(cn, bundle))
-						.filter(c -> c != null).collect(Collectors.toList()))
-				.orElse(Collections.emptyList()).stream().map(c -> (Class<?>) c).collect(Collectors.toList());
-		
-		//if a class is opened and has a stepping rule in the xDSL's interpreter, 
-		//the objects of the class are coverable
-		for (Class<?> clazz : classes) {
-			String className = clazz.getDeclaredAnnotation(Aspect.class).className().getName();
-			className = className.substring(className.lastIndexOf(".") + 1);
-			Method[] methods = clazz.getDeclaredMethods();
-			for (Method method: methods) {
-				if (method.getAnnotationsByType(Step.class).length > 0) {
-					extendedClassesWithStep.add(className);
-					coverableClasses.add(className);
-					break;
-				}
-			}
-			//if the extended class had no @step rule
-			if (!extendedClassesWithStep.contains(className)) {
-				extendedClassesWithoutStep.add(className);
-			}
-		}
-	}
-	
-	private void findAleClasses(Dsl dsl, Bundle bundle) {
-		String aleFilePath = dsl.getEntry("ale").getValue().replaceFirst("resource", "plugin");
-		Resource aleResource = (new ResourceSetImpl()).getResource(URI.createURI(aleFilePath), true);
-		Unit interpreter = (Unit) aleResource.getContents().get(0);
-		List<BehavioredClass> classes = interpreter.getXtendedClasses();
-		//if a class is opened and has a stepping rule in the xDSL's interpreter, 
-		//the objects of the class are coverable
-		for (BehavioredClass clazz : classes) {
-			EList<Operation> operations = clazz.getOperations();
-			String className = clazz.eClass().getName();
-			for (int i=0; i<operations.size(); i++) {
-				for (Tag tag:operations.get(i).getTag()) {
-					if (tag.getName().equals("step")) {
-						extendedClassesWithStep.add(className);
-						coverableClasses.add(className);
-						i = operations.size();
-						break;
-					}
-				}
-			}
-			//if the extended class had no @step rule
-			if (!extendedClassesWithStep.contains(className)) {
-				extendedClassesWithoutStep.add(className);
-			}
-		}
-	}
-	private Class<?> loadClass(String className, Bundle bundle) {
-		Class<?> result = null;
-		try {
-			result = bundle.loadClass(className.replaceAll("\\s", "").trim());
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		return result;
 	}
 	
 	//for each class that is not coverable (it is not extended in the interpreter)
@@ -183,12 +94,12 @@ public class TDLCoverageUtil {
 	public void checkInheritanceForNotCoverableClasses() {
 		for (EClassifier clazz: metamodelRootElement.getEClassifiers()) {
 			String className = clazz.getName();
-			if (clazz instanceof EClass) {
+			if (clazz instanceof EClass eclazz) {
 				if (!coverableClasses.contains(className) && 
 						!extendedClassesWithoutStep.contains(className)) {
-					checkInheritance((EClass) clazz);
+					checkInheritance(eclazz);
 				}
-				checkDynamicAspectsOfClass(clazz);	
+				checkDynamicAspectsOfClass(eclazz);	
 			}
 		}	
 	}
@@ -220,7 +131,8 @@ public class TDLCoverageUtil {
 	
 	private boolean isDynamicFeature(EStructuralFeature feature) {
 		List<EAnnotation> featureDynamicAnnotations = feature.getEAnnotations().stream().
-				filter(a -> a.getSource().equals("dynamic") || a.getSource().equals("aspect")).collect(Collectors.toList());
+				filter(a -> a.getSource().equals("dynamic") || 
+						a.getSource().equals("aspect")).collect(Collectors.toList());
 		return (featureDynamicAnnotations != null && featureDynamicAnnotations.size() > 0);
 	}
 
@@ -277,6 +189,13 @@ public class TDLCoverageUtil {
 		return dslSpecificCoverage;
 	}
 	
+	public IDSLSpecificCoverage getDslSpecificCoverageExtension() {
+		if (dslSpecificCoverageExtension == null) {
+			findDSLSpecificCoverage();
+		}
+		return dslSpecificCoverageExtension;
+	}
+	
 	private void findDSLSpecificCoverage() {
 		//check if there is a DSL-Specific coverage extension
 		DSLSpecificCoverageHandler dslSpecificCoverageHandler = new DSLSpecificCoverageHandler();
@@ -296,9 +215,6 @@ public class TDLCoverageUtil {
 	}
 	
 	private Resource loadDSLSpecificCoverageFile() {
-//		ResourceSet resourceSet = new ResourceSetImpl();
-//		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("cov", new XMIResourceFactoryImpl());
-//		resourceSet.getPackageRegistry().put(DSLSpecificCoveragePackage.eNS_URI, DSLSpecificCoveragePackage.eINSTANCE);
 		String coverageFilePath = getCoverageFilePath();
 		if (coverageFilePath != null) {
 			Resource coverageFileResource = (new ResourceSetImpl()).getResource(URI.createURI(coverageFilePath), true);
@@ -308,18 +224,11 @@ public class TDLCoverageUtil {
 	}
 	
 	private String getCoverageFilePath() {
-		final Resource res = (new ResourceSetImpl()).getResource(URI.createURI(DSLPath), true);
-		final Dsl dsl = (Dsl) res.getContents().get(0);
-		if (dsl.getEntry("coverageFilePath") != null) {
-			return dsl.getEntry("coverageFilePath").getValue().replaceFirst("resource", "plugin");
+		String path = dslProcessor.getPath2CoverageRules();
+		if (path != null) {
+			path = path.replaceFirst("resource", "plugin");
+			return path;
 		}
 		return null;
-	}
-	
-	public IDSLSpecificCoverage getDslSpecificCoverageExtension() {
-		if (dslSpecificCoverageExtension == null) {
-			findDSLSpecificCoverage();
-		}
-		return dslSpecificCoverageExtension;
 	}
 }
