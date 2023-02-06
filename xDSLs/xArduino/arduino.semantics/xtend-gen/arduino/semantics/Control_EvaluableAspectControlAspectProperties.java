c36150fe20607e8127f8eaaf4c4083b6bf478ca6
Faezeh Khorram <khorram.faezeh@gmail.com>
2023-02-06 17:03:32 +0100

    delete xtend-gen

--- xDSLs/xArduino/arduino.semantics/xtend-gen/arduino/semantics/Control_EvaluableAspectControlAspectProperties.java
+++
@@ -1,5 +0,0 @@
-package arduino.semantics;
-
-@SuppressWarnings("all")
-public class Control_EvaluableAspectControlAspectProperties {
-}
--- xDSLs/xArduino/arduino.semantics/xtend-gen/arduino/semantics/Utilities_ExecutableAspectUtilitiesAspectProperties.java
+++
@@ -1,5 +0,0 @@
-package arduino.semantics;
-
-@SuppressWarnings("all")
-public class Utilities_ExecutableAspectUtilitiesAspectProperties {
-}
